e5f4b719ad9856457d91172f881ec282e1b35bf3
Rachael Webster <ancient.void@gmail.com>
2025-07-29 18:57:48 -0700

    Added base class for ingame objects.

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from constants import *
 def main():
     pygame.init()
 
+    clock = pygame.time.Clock()
+    dt = 0 # delta time
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     while True:
@@ -13,6 +16,7 @@ def main():
         screen.fill("black")
         # game loop goes here
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 
 
