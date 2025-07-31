79c3f5ccc4bcf13f6f1314dfb75418e7f9853e55
Rachael Webster <ancient.void@gmail.com>
2025-07-31 01:09:09 -0700

    Fixed error in player initiation: removed from main loop. Added turning functionality.

--- main.py
+++ main.py
@@ -4,34 +4,34 @@ from circleshape import *
 from player import *
 
 def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
     pygame.init()
 
     clock = pygame.time.Clock()
     dt = 0 # delta time
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    player = Player(
+        SCREEN_WIDTH / 2, 
+        SCREEN_HEIGHT / 2,
+        PLAYER_RADIUS)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         
-        player = Player(
-            SCREEN_WIDTH / 2, 
-            SCREEN_HEIGHT / 2,
-            PLAYER_RADIUS)
+
+        
+        player.update(dt)
         player.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60)/1000
-
-
-
-
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
