76d03b682dbd78a1a1c6d16322f4de5a496cc04b
Rachael Webster <ancient.void@gmail.com>
2025-07-31 23:04:14 -0700

    Added ability to shoot

--- main.py
+++ main.py
@@ -21,11 +21,13 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    bullets = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
-
+    Shot.containers = (bullets, updatable, drawable)
+    
     # Initializing Player
     player = Player(
         SCREEN_WIDTH / 2, 
--- player.py
+++ player.py
@@ -1,11 +1,13 @@
 import pygame
 from constants import *
 from circleshape import *
+from bullets import *
 
 class Player(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
         self.rotation = 0
+        self.shot_timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -25,9 +27,14 @@ class Player(CircleShape):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
+        # movement
         if keys[pygame.K_a]:
             self.rotate(dt)
         if keys[pygame.K_d]:
@@ -36,3 +43,12 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+
+        # shooting (handles cooldown)
+        if self.shot_timer > 0:
+            self.shot_timer -= dt
+        elif keys[pygame.K_SPACE]:
+            self.shoot()
+            self.shot_timer = PLAYER_SHOOT_COOLDOWN
+
+
